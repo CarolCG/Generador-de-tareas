9ceb37e36d2859d8cbdadceb0442d7de6d64f466
Carolina Castro <caritobel1223@gmail.com>
2023-01-11 18:20:44 +0000

    generadorcartas

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,7 +7,7 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 	const [tareas, setTareas] = useState("")
 	const [listaTareas, setlistaTareas] = useState([])
-	// const [eliminarTareas, seteliminarTareas] = useState(listaTareas)
+	const [noTareas, setnoTareas] = useState("")
 
 	function handleTareas (e) {
 		if(e.key === "Enter") {
@@ -16,21 +16,26 @@ const Home = () => {
 				alert("Falta informacion")
 			}else{setlistaTareas([...listaTareas,tareas])
 			console.log(listaTareas)
+			setnoTareas("visually-hidden")
 		}
 			
 		}
 
 	}
 	function eliminarTareas(id){
-		setlistaTareas(lista.filter((tarea,index) => index !== id))
+		setlistaTareas(listaTareas.filter((tarea,index) => index !== id))
+		if(listaTareas.length === 1){
+			setnoTareas("")
+		}
+
 	}
 	return (
 		<div className="container text-center">
 			<h1 className="fw-light ">Todos</h1>
 		<ul className="list-group list-group-flush">
-  <li className="list-group-item fw-light"> <input className="" type="text" placeholder="What needs to be done?" onChange={(e)=> setTareas(e.target.value)} value={tareas} onKeyDown={handleTareas}></input></li>
-  {listaTareas.map((tarea, id) => <li className="list-group-item fs-5" key={id}>{tarea}</li>)}
-  {/* {eliminarTareas.filter((tarea, id) => <li className="list-group-item fs-5" key={id}>{tarea}</li>)} */}
+  <li className="list-group-item fw-light"> <input className="w-100 border border-0 fs-5" type="text" placeholder="What needs to be done?" onChange={(e)=> setTareas(e.target.value)} value={tareas} onKeyDown={handleTareas} ></input></li>
+  <li className={"list-group-item  fw-light "+ noTareas}>No hay tareas, añadir tareas</li>
+  {listaTareas.map((tarea, id) => <li id="cruz" className="list-group-item fs-5 d-flex justify-content-between"  key={id}>{tarea}<button id="eliminarcruz" type="button" className="btn-close fs-6 mt-1 " aria-label="Close" onClick={()=> eliminarTareas(id)}></button></li>)}
   <li className="list-group-item  fw-light">{listaTareas.length} item left</li>
 </ul>
 </div>
